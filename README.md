73dc00f03c90229ed77db9ffbc893520f7cc6e10
Shy Aberman <shy@ripl.com>
2019-02-21 12:22:00 -0800

    Add in AWS provider

--- README.md
+++ README.md
@@ -5,4 +5,6 @@
 1. clone repo and cd into it
 1. `terraform init`
 1. `terraform get`
+1. `terraform providers`
 
+If you see `provider.aws ~> 1.36`, you are good to go!
--- main.tf
+++ main.tf
@@ -2,3 +2,9 @@ terraform {
   // We lock the terraform version because new versions are basically never backward-compatible.
   required_version = "~> 0.11.0"
 }
+
+provider "aws" {
+  region = "${var.aws_region}"
+  // We lock the provider version because new versions are basically never backward-compatible.
+  version = "~> 1.36"
+}
